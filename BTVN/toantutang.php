78c38885cb9fcee464d130813430468e235915d8
nganhs0312 <nganhs0312@gmail.com>
2025-12-17 23:46:04 +0700

    Create toantutang.php

---
+++ BTVN/toantutang.php
@@ -0,0 +1,14 @@
+<?php
+$a = 10;
+$b = 3;
+
+echo "a = $a, b = $b<br><br>";
+
+echo "a + b = " . ($a + $b) . "<br>";
+echo "a - b = " . ($a - $b) . "<br>";
+echo "a * b = " . ($a * $b) . "<br>";
+echo "a / b = " . ($a / $b) . "<br><br>";
+
+echo "++a = " . (++$a) . "<br>";
+echo "b++ = " . ($b++) . "<br>";
+echo "b sau khi tăng = " . $b;
